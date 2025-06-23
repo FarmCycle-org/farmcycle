8ad10bf82dba65b7a6a216245c4d082a8c16789b
gurnoorkaur-13 <justgunno@gmail.com>
2025-06-23 13:21:24 +0530

    Implement user authentication

--- server/models/User.js
+++ server/models/User.js
@@ -7,11 +7,20 @@ const userSchema = new mongoose.Schema({
   role: { type: String, enum: ['provider', 'collector'], required: true },
   contact: String,
   location: {
-    type: { type: String, default: "Point" },
-    coordinates: [Number], // [lng, lat]
+  type: {
+    type: String,
+    enum: ['Point'],
+    // required:true,
+    // default: 'Point'
+  },
+  coordinates: {
+    type: [Number],
+    // required:true,
+
   }
+}
 }, { timestamps: true });
 
-userSchema.index({ location: "2dsphere" });
+userSchema.index({ location: "2dsphere" },{ sparse: true }); //index will only include documents that have the location field. If a document doesn't have a location field, it won't be indexed.
 
 module.exports = mongoose.model("User", userSchema);
